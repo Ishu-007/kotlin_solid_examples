50333da92d7c18799e863d071e1de99415cae1e2
ishu.sharma <71206604+Ishu-007@users.noreply.github.com>
2025-05-10 10:01:04 +0530

    single responsibility principle

---
+++ SRP/RestaurantManager_Bad.kt
@@ -0,0 +1,48 @@
+// SRP stands for Single Responsibility Principle and suggests us that there should'n be more than one reason for a class to change.
+
+/**
+ * File: RestaurantManager_Bad.kt
+ * Purpose: Demonstrates a violation of the Single Responsibility Principle (SRP)
+ * 
+ * Description:
+ * This example shows a class that handles multiple responsibilities such as 
+ * taking orders, preparing food, serving food — violating SRP.
+ * 
+ * Author: Ishu Sharma
+ * Date: May 5th, 2025
+ */
+
+class RestaurantManager {
+
+    // taking orders from customer
+    fun takeOrder () {
+        println("Taking Order.")
+    }
+
+    // preparing the food for the order
+    fun prepareFood () {
+        println("Preparing Food.")
+    }
+
+    // serving the food for the order
+    fun serveFood () {
+        println("Serving Food.")
+    }
+
+    // creating the invoice
+    fun createInvoice () {
+        println("Creating Invoice.")
+    }
+
+}
+
+fun main () {
+
+    val manager = RestaurantManager()
+
+    manager.takeOrder()
+    manager.prepareFood()
+    manager.serveFood()
+    manager.createInvoice()
+
+}
